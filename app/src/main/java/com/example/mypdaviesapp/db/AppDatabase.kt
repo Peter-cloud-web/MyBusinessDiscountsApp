fc8eab1ce2e67ab5c61d96327295ca46f45a5904
Peter-cloud-web <petergichia165@gmail.com>
2025-07-05 14:22:11 +0300

    Create database

---
+++ app/src/main/java/com/example/mypdaviesapp/db/AppDatabase.kt
@@ -0,0 +1,23 @@
+package com.example.mypdaviesapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.mypdaviesapp.dao.BarcodeDao
+import com.example.mypdaviesapp.dao.CleaningHistoryDao
+import com.example.mypdaviesapp.dao.ClientDao
+import com.example.mypdaviesapp.entities.Barcode
+import com.example.mypdaviesapp.entities.CleaningHistory
+import com.example.mypdaviesapp.entities.Client
+
+@Database(
+    entities = [Client::class, Barcode::class, CleaningHistory::class],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun clientDao(): ClientDao
+    abstract fun barcodeDao(): BarcodeDao
+    abstract fun cleaningHistoryDao(): CleaningHistoryDao
+}
